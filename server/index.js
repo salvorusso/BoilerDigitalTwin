c8e80557000f46d37479dcfcd7d8148e02eea556
Salvo Russo <salvatore.russo01@leonardocompany.com>
2023-06-22 23:51:50 +0200

    Opc ua server done

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 /*global require,setInterval,console */
 const opcua = require("node-opcua");
 const service = require("./modules/service")
-// Let's create an instance of OPCUAServer
+
 const server = new opcua.OPCUAServer({
-    port: 4334, // the port of the listening socket of the server
-    resourcePath: "/UA/MyLittleServer", // this path will be added to the endpoint resource name
-     buildInfo : {
-        productName: "MySampleServer1",
-        buildNumber: "7658",
-        buildDate: new Date(2014,5,2)
+    port: 4334,
+    resourcePath: "/UA/LM-32",
+    buildInfo: {
+        productName: "BensonBoilerOPCUA",
+        buildNumber: "1",
+        buildDate: new Date()
     }
 });
 
 function post_initialize() {
-    console.log("initialized");
-    service.construct_my_address_space(server);
-    server.start(function() {
+    console.log("Initializing...");
+    service.constructAddressSpace(server);
+    server.start(function () {
         console.log("Server is now listening ... ( press CTRL+C to stop)");
-        console.log("port ", server.endpoints[0].port);
+        console.log("Port ", server.endpoints[0].port);
         const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
-        console.log(" the primary server endpoint url is ", endpointUrl );
+        console.log("Primary server endpoint url", endpointUrl);
+        // Aggiorna i valori in batch
+        setInterval(() => {
+            service.updateVariables(server)
+        }, 1000)
     });
 }
 server.initialize(post_initialize);
--- server/modules/service.js
+++ server/modules/service.js
@@ -1,147 +1,71 @@
 const opcua = require("node-opcua");
-const http = require('http');
+const axios = require('axios');
 
-function getValue(path) {
-    http.get(`http://localhost:3000${path}`, (res) => {
-        if (res.statusCode !== 200) {
-          console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-          res.resume();
-          return;
-        }
-      
-        let data = '';
-      
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-      
-        res.on('close', () => {
-          console.log('Retrieved all data');
-          console.log(JSON.parse(data));
-          console.log("fuck ", JSON.parse(data).caloreDisperso)
-          return JSON.parse(data).caloreDisperso
-        });
-      });
+// Mappa dei nodi osservati dal server
+// Il valore indica il BrowseName
+const nodes = {
+    "Boiler": {
+        caloreDisperso: "Calore Disperso",
+        temperaturaAcquaIngresso: "Temperatura Acqua in Ingresso",
+        caloreDisperso: "Calore Disperso",
+        efficienza: "Efficienza",
+        flussoAcquaIngresso: "Flusso Acqua in Ingresso",
+        temperaturaVaporeUscita: "Temperatura Vapore in Uscita"
+    },
+    "Generatore Elettrico": {
+        potenzaElettrica: "Potenza Elettrica"
+    }
+
 }
 
 function generateVariable(object, browseName, value) {
-return {
-    componentOf: object,
-    browseName: browseName,
-    dataType: "Double",
-    value: {
-        get: function () {
-            return new opcua.Variant({dataType: opcua.DataType.Double, value: getValue(value) });
-        }
-    }   
-}
+    return {
+        componentOf: object,
+        browseName: browseName,
+        dataType: "Double",
+        nodeId: `s=${value}`,
+    }
 }
 
-function construct_my_address_space(server) {
-
+function constructAddressSpace(server) {
     const addressSpace = server.engine.addressSpace;
     const namespace = addressSpace.getOwnNamespace();
 
-    //declare a new object
-    const device = namespace.addObject({
-        organizedBy: addressSpace.rootFolder.objects,
-        browseName: "MyDevice"
-    });
-
-    const boiler = namespace.addObject({
-        organizedBy: addressSpace.rootFolder.objects,
-        browseName: "Boiler"
-    });
-
-    let temperaturaAcqua = 25;
-    namespace.addVariable({
-        componentOf: boiler,
-        browseName: "Temperatura Acqua in Ingresso",
-        dataType: "Double",
-        value: {
-            get: function () {
-                return new opcua.Variant({dataType: opcua.DataType.Double, value: temperaturaAcqua });
-            }
+    for (const object in nodes) {
+        const obj = namespace.addObject({
+            organizedBy: addressSpace.rootFolder.objects,
+            browseName: object
+        })
+        for (const node in nodes[object]) {
+            namespace.addVariable(generateVariable(obj, nodes[object][node], node))
+            console.log(`Added Node: nodeId:ns=1;s=${node} , BrowseName:${nodes[object][node]}`);
         }
-    })
-
-    let caloreDisperso = 20000;
-    namespace.addVariable(generateVariable(boiler, "Calore Disperso", "/CaloreDisperso"))
-    // namespace.addVariable({
-    //     componentOf: boiler,
-    //     browseName: "Calore Disperso",
-    //     dataType: "Double",
-    //     value: {
-    //         get: function () {
-    //             return new opcua.Variant({dataType: opcua.DataType.Double, value: caloreDisperso });
-    //         }
-    //     }        
-    // })
-
-    // add some variables
-    // add a variable named MyVariable1 to the newly created folder "MyDevice"
-    let variable1 = 1;
-    
-    // emulate variable1 changing every 500 ms
-    setInterval(function(){  variable1+=1; }, 500);
-    
-    namespace.addVariable({
-        componentOf: device,
-        browseName: "MyVariable1",
-        dataType: "Double",
-        value: {
-            get: function () {
-                return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
-            }
-        }
-    });
-    
-    // add a variable named MyVariable2 to the newly created folder "MyDevice"
-    let variable2 = 10.0;
-    
-    namespace.addVariable({
-    
-        componentOf: device,
-    
-        nodeId: "ns=1;b=1020FFAA", // some opaque NodeId in namespace 4
-    
-        browseName: "MyVariable2",
-    
-        dataType: "Double",    
-    
-        value: {
-            get: function () {
-                return new opcua.Variant({dataType: opcua.DataType.Double, value: variable2 });
-            },
-            set: function (variant) {
-                variable2 = parseFloat(variant.value);
-                return opcua.StatusCodes.Good;
-            }
-        }
-    });
-    const os = require("os");
-    /**
-     * returns the percentage of free memory on the running machine
-     * @return {double}
-     */
-    function available_memory() {
-        // var value = process.memoryUsage().heapUsed / 1000000;
-        const percentageMemUsed = os.freemem() / os.totalmem() * 100.0;
-        return percentageMemUsed;
     }
-    namespace.addVariable({
-    
-        componentOf: device,
-    
-        nodeId: "s=free_memory", // a string nodeID
-        browseName: "FreeMemory",
-        dataType: "Double",    
-        value: {
-            get: function () {return new opcua.Variant({dataType: opcua.DataType.Double, value: available_memory() });}
+}
+
+async function updateVariable(server, path) {
+    try {
+        const response = await axios.get(`http://localhost:3000/${path}`); // URL del simulatore di dati
+        const value = response.data[path];
+
+        // Aggiorna il valore della variabile OPC UA
+        const variable = server.engine.addressSpace.findNode(`ns=1;s=${path}`);
+        variable.setValueFromSource(new opcua.Variant({ dataType: opcua.DataType.Double, value }));
+    } catch (error) {
+        console.error("Errore durante la richiesta HTTP:", error);
+    }
+}
+
+async function updateVariables(server) {
+    for (const object in nodes) {
+        for (const node in nodes[object]) {
+            updateVariable(server, node)
         }
-    });
+    }
 }
 
 module.exports = {
-    construct_my_address_space
+    constructAddressSpace,
+    updateVariables,
+    nodes
 }
